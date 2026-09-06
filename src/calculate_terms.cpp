83ee849d3fdfc17348ac3b4ef7d2a7f6e548eb05
Diogo-Laptop <YOUR_NOREPLY_EMAIL>
2026-09-06 13:50:49 +0100

    function terms + power

--- src/calculate_terms.cpp
+++ src/calculate_terms.cpp
@@ -15,17 +15,20 @@ static void skip_spaces(std::string::iterator &it)
 }
 
 
+
+
 static int get_element_term(std::string::iterator &it, std::string equation)
 {
-	int sign = 0;
+	int sign = 1;
 	std::string aux_for_number;
 
-	if (*it == '-')
-		sign = -1;
-	else
+	if (*it == '+')
+		it++;
+	else if (*it == '-')
+	{
 		sign = 1;
-	
-	it++;
+		it++;
+	}
 
 	skip_spaces(it);
 
@@ -36,6 +39,8 @@ static int get_element_term(std::string::iterator &it, std::string equation)
 
 	return std::stoi(aux_for_number) * sign;
 }
+
+
 
 void calculate_terms(Equation &Eq)
 {
@@ -55,18 +60,41 @@ void calculate_terms(Equation &Eq)
 			continue;
 		}
 
-		else if (*it == '-' || *it == '+' || std::isdigit(static_cast<unsigned char>(*it)))
+		// Parsing each term  A * X^B
+		if (*it == '-' || *it == '+' || std::isdigit(static_cast<unsigned char>(*it)))
 		{
+			// Getting the Term A first after the sign
 			element.term = get_element_term(it, Eq.equation);
-			current_terms->push_back(element);
+			
 
+			// After getting the Term, we skip towards a single *, and skip again to to waiting for an X (variable raised to a power B)
 			skip_spaces(it);
-
 			if (*it != '*')
 				throw std::invalid_argument("No * after term number " + std::to_string(element.term));
+			it++;
+			skip_spaces(it);
+
+			// Now allowing spaces in between X and ^ and B
+			if (*it != 'X')
+				throw std::invalid_argument("No X after " + std::to_string(element.term) + " *");
+			it++;
+			if (*it != '^')
+				throw std::invalid_argument("No ^ after " + std::to_string(element.term) + " X*");
+			it++;
+
+			std::string aux_for_number;
+			for ( ; std::isdigit(static_cast<unsigned char>(*it)) && it != Eq.equation.end(); it++)
+				aux_for_number += *it;
 
 
+			std::cout << aux_for_number << std::endl;
+			element.power = std::stoi(aux_for_number);
+
+			current_terms->push_back(element);
 		}
+		else
+			throw std::invalid_argument("Not expected character");
+
 	}
 }
 
